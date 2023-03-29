3b27436fc940ec2d6919ac85a7c4f95292c22e1a
Enrique Rojas R <61429959+EnriqueRojasR@users.noreply.github.com>
2023-03-29 17:29:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Configuración de la conexión a la base de datos MongoDB
 
-client = MongoClient("MONGO_URI")
+client = MongoClient("mongodb+srv://User:user123@apipokemon.zlp2gxc.mongodb.net/?retryWrites=true&w=majority")
 db = client.pokedex
 collection = db.pokemons
 
